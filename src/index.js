9ded9acfa52400cd980c849f013ff1e116848308
aahvocado <dan.dan.makes.stuff@gmail.com>
2020-09-06 11:29:31 -0700

    check for environment instead of localhost

--- src/index.js
+++ src/index.js
@@ -3,14 +3,16 @@ import https from 'https';
 
 import serverController from 'controllers/serverController';
 
-const SERVER_PATH = process.env['SERVER_PATH'];
-const HOST = process.env['HOST'];
-const PORT = process.env['PORT'];
+const ENVIRONMENT = process.env['ENVIRONMENT'];
 const CERT_PATH = process.env['CERT_PATH'];
 const CERT_KEY_PATH = process.env['CERT_KEY_PATH'];
 
+const SERVER_PATH = process.env['SERVER_PATH'];
+const HOST = process.env['HOST'];
+const PORT = process.env['PORT'];
+
 // local
-if (HOST === 'localhost') {
+if (ENVIRONMENT === 'local') {
   serverController.listen(PORT, HOST, () => {
     console.log(`Server listening on http://${HOST}:${PORT}${SERVER_PATH}`);
   });

078012a99e7ca27a4ace2fe0f73c0a993d386843
aahvocado <dan.dan.makes.stuff@gmail.com>
2020-09-06 11:25:48 -0700

    support https certificate getting

--- src/index.js
+++ src/index.js
@@ -1,8 +1,29 @@
+import fs from 'fs';
+import https from 'https';
+
 import serverController from 'controllers/serverController';
 
 const SERVER_PATH = process.env['SERVER_PATH'];
 const HOST = process.env['HOST'];
 const PORT = process.env['PORT'];
-serverController.listen(PORT, HOST, () => {
-  console.log(`Server listening on http://${HOST}:${PORT}/${SERVER_PATH}`);
-});
+const CERT_PATH = process.env['CERT_PATH'];
+const CERT_KEY_PATH = process.env['CERT_KEY_PATH'];
+
+// local
+if (HOST === 'localhost') {
+  serverController.listen(PORT, HOST, () => {
+    console.log(`Server listening on http://${HOST}:${PORT}${SERVER_PATH}`);
+  });
+
+// production
+} else {
+  const server = https.createServer({
+    cert: fs.readFileSync(CERT_PATH),
+    key: fs.readFileSync(CERT_KEY_PATH),
+  }, serverController);
+
+  server.listen(PORT, HOST, () => {
+    console.log(`Server listening on https://${HOST}:${PORT}${SERVER_PATH}`);
+  });
+}
+
